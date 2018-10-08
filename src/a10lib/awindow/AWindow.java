77fb9f478171b9101ff4de2b9507eefcf5622c37
athensclub <43963760+athensclub@users.noreply.github.com>
2018-10-08 23:13:04 +0700

    add document to awindow

--- src/a10lib/awindow/AWindow.java
+++ src/a10lib/awindow/AWindow.java
@@ -13,56 +13,100 @@ public class AWindow implements Runnable {
 
 	private float fps;
 
+	/**
+	 * Default FPS of every AWindow
+	 */
 	public static final float DEFAULT_FPS = 30;
 
+	/**
+	 * Create new instance of AWindow having title and AStage
+	 * @param title: the title of this AWindow
+	 * @param s: the AStage that this AWindow will contain
+	 */
 	public AWindow(String title, AStage s) {
 		this(title);
 		frame.setStage(s);
 	}
 
+	/**
+	 * Create new instance of AWindow having title
+	 * @param title: the title of this AWindow
+	 */
 	public AWindow(String title) {
 		frame = new AFrame(title);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setFPS(AWindow.DEFAULT_FPS);
 	}
 
+	/**
+	 * Create new instance of AWindow
+	 */
 	public AWindow() {
 		this("");
 	}
 
+	/**
+	 * Set the title of this AWindow
+	 * @param str: the new title of this window 
+	 */
 	public void setTitle(String str) {
 		frame.setTitle(str);
 	}
 
+	/**
+	 * Get the current title of this AWindow
+	 * @return the current title of this AWindow
+	 */
 	public String getTitle() {
 		return frame.getTitle();
 	}
 
+	/**
+	 * Set Icon image of this AWindow
+	 * @param img the icon image of this AWindow
+	 */
 	public void setIcon(AImage img) {
 		frame.setIcon(img);
 	}
 
-	public Image getIconImage() {
-		return frame.getIconImage();
-	}
-
+	/**
+	 * Get the current icon image of this AWindow
+	 * @return the current icon image of this AWindow
+	 */
 	public AImage getIcon() {
 		return frame.getIcon();
 	}
 
+	/**
+	 * Set new size of this AWindow
+	 * @param w: the new width of this AWindow
+	 * @param h: the new height of this AWindow
+	 */
 	public void setSize(int w, int h) {
 		frame.getPanel().setPreferredSize(new Dimension(w, h));
 		frame.pack();
 	}
 
+	/**
+	 * Get the current width of this AWindow
+	 * @return the current width of this AWindow
+	 */
 	public int getWidth() {
 		return frame.getPanel().getWidth();
 	}
 
+	/**
+	 * Get the current height of this AWindow
+	 * @return the current height of this AWindow
+	 */
 	public int getHeight() {
 		return frame.getPanel().getHeight();
 	}
 
+	/**
+	 * Set the stage that this AWindow will contain
+	 * @param s: the stage that this AWindow will contain
+	 */
 	public void setStage(AStage s) {
 		if (frame.getStage() != null) {
 			frame.getStage().setWindow(null);
@@ -71,18 +115,34 @@ public class AWindow implements Runnable {
 		frame.setStage(s);
 	}
 
+	/**
+	 * Set the frame rate of this AWindow
+	 * @param f: the frame rate of this AWindow
+	 */
 	public void setFPS(float f) {
 		fps = f;
 	}
 
+	/**
+	 * Get the current frame rate of this AWindow
+	 * @return the current frame rate of this AWindow
+	 */
 	public float getFPS() {
 		return fps;
 	}
 
+	/**
+	 * Get the current AStage of this AWindow
+	 * @return the current AStage of this AWindow
+	 */
 	public AStage getStage() {
 		return frame.getStage();
 	}
 
+	/**
+	 * Create new instance of AWindow having AStage
+	 * @param s: the stage that this AWindow will contain
+	 */
 	public AWindow(AStage s) {
 		this("", s);
 	}
@@ -110,6 +170,9 @@ public class AWindow implements Runnable {
 
 	}
 
+	/**
+	 * Start Running and Showing this AWindow
+	 */
 	public void start() {
 		frame.setResizable(false);
 		frame.setVisible(true);
